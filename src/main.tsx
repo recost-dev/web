481c59f2b95e76a92aafe19fc39e6ef03c65f8e8
AndresL230 <190146319+AndresL230@users.noreply.github.com>
2026-03-20 00:38:58 -0400

    fixed font

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource-variable/geist'
+import '@fontsource-variable/geist-mono'
 import '../app/globals.css'
 import App from './App'
 
