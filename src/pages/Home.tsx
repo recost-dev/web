bae252cf679a46e4e40524003399ca513aa12c23
Andres Lopez <190146319+AndresL230@users.noreply.github.com>
2026-03-20 14:55:13 -0400

    dashboard hidden mobile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <HeroSection />
       <HowItWorks />
       <FeaturesSection />
-      <DashboardPreview />
+      <div className="hidden md:block"><DashboardPreview /></div>
       <OpenSourceSection />
 <Footer />
     </main>
