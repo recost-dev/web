0b2927eca5be10926687440a8a6177b41b8fcf34
Aslan Wang <aslanwang187@gmail.com>
2026-03-20 14:43:41 -0400

    zoom fix on ios

--- src/main.tsx
+++ src/main.tsx
@@ -4,4 +4,9 @@ import '@fontsource-variable/geist-mono'
 import '../app/globals.css'
 import App from './App'
 
+// Disable pinch-to-zoom on iOS (viewport meta user-scalable=no is ignored since iOS 10)
+document.addEventListener('touchmove', (e) => {
+  if (e.touches.length > 1) e.preventDefault()
+}, { passive: false })
+
 createRoot(document.getElementById('root')!).render(<App />)
